c143151e14b4eb51216328278396c1635d9fb8ae
Yacklab <yacklabtech@gmail.com>
2020-04-05 17:06:07 +0200

    add sync url on name input

--- src/containers/search-input.tsx
+++ src/containers/search-input.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TextField, { TextFieldProps } from "@material-ui/core/TextField";
 import { useInputValue } from "../hooks/use-input-value";
 import { useSelector, useDispatch } from "react-redux";
@@ -9,8 +9,17 @@ import {
 
 const SearchInput: React.FC<TextFieldProps> = props => {
   const name = useSelector(selectNameParam);
-  const handler = useInputValue(name);
   const dispatch = useDispatch();
+  const [value, setValue] = useState(name);
+  useEffect(() => {
+    setValue(name);
+  }, [name]);
+
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setValue(e.currentTarget.value);
+  };
 
   return (
     <TextField
@@ -18,10 +27,11 @@ const SearchInput: React.FC<TextFieldProps> = props => {
       onKeyDown={e => {
         if (e.keyCode === 13) {
           e.preventDefault();
-          dispatch(setSearchParam({ name: "name", value: handler.value }));
+          dispatch(setSearchParam({ name: "name", value }));
         }
       }}
-      {...handler}
+      value={value}
+      onChange={onChange}
       spellCheck="false"
       size="small"
       label="Search by name"
